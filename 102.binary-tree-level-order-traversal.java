3e83d41aee3b68199932878558b3bcd6654908bc
Adarsh Vishwakarma <163298836+adarsh-vishwakarma@users.noreply.github.com>
2024-11-14 13:09:25 +0530

    14/11/2024

---
+++ 102.binary-tree-level-order-traversal.java
@@ -0,0 +1,59 @@
+
+/*
+ * @lc app=leetcode id=102 lang=java
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+
+        List<Integer> currLevel = new ArrayList<>();
+
+        while(!queue.isEmpty()) {
+            TreeNode currNode = queue.remove();
+            if(currNode == null) {
+                result.add(new ArrayList<>(currLevel));
+                currLevel.clear();
+                if(!queue.isEmpty()){
+                    queue.add(null);
+                }
+            } else {
+                currLevel.add(currNode.val);
+
+                if(currNode.left != null) queue.add(currNode.left);
+                if(currNode.right != null) queue.add(currNode.right);
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
